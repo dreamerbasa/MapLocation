3a2d7eb855c1eb12f575c5cad5e5486185194a25
dreamerbasa <40892228+dreamerbasa@users.noreply.github.com>
2019-01-20 03:03:41 +0530

    Initial commit

---
+++ app/src/main/java/com/google/maplocation/DownloadUrl.java
@@ -0,0 +1,54 @@
+package com.google.maplocation;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class DownloadUrl {
+
+    public String readUrl(String strurl) throws IOException{
+
+        String data = "";
+        InputStream is = null;
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(strurl);
+            // Creating an http connection to communicate with url
+            urlConnection = (HttpURLConnection) url.openConnection();
+            //connecting to url
+            urlConnection.connect();
+
+            //reading data from url
+            is = urlConnection.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+            StringBuilder sb = new StringBuilder();
+            String line = "";
+
+            while ((line = br.readLine())!=null){
+
+                sb.append(line);
+
+            }
+
+            data = sb.toString();
+            Log.d("DownloadUrl",data);
+            br.close();
+
+        }catch (Exception e){
+            Log.d("Exception",e.toString());
+        }
+        finally {
+            is.close();
+            urlConnection.disconnect();
+        }
+        return data;
+    }
+
+}
